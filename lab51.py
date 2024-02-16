4030ede9e052eb57a55c49f90f7fe1ddceec0b89
ToxaRat <toxarat@gmail.com>
2024-02-16 19:09:35 +0200

    1

---
+++ lab51.py
@@ -0,0 +1,21 @@
+def caching_fibonacci():
+    #Створити порожній словник cache
+    cache = dict()
+
+    def fibonacci(n):
+        if n <= 0: return 0
+        if n == 1: return 1
+        if n in cache: return cache[n]
+
+        cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+        return cache[n]
+
+    return fibonacci
+
+
+# Отримуємо функцію fibonacci
+fib = caching_fibonacci()
+
+# Використовуємо функцію fibonacci для обчислення чисел Фібоначчі
+print(fib(10))  # Виведе 55
+print(fib(15))  # Виведе 610
